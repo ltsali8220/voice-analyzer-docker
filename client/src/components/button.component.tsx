fa4121185cb4e14f6c1cd60486039d14fbf47b92
senthil-k8s <senthilathiban0@gmail.com>
2024-01-27 18:01:51 +0530

    bug fix

--- client/src/components/button.component.tsx
+++ client/src/components/button.component.tsx
@@ -5,7 +5,7 @@ const Button: React.FC<ButtonProps> = ({ title, onClick, color }) => {
     return (
         <div>
             <button
-                className={`border-2 p-2 rounded-lg bg-${color}-400 hover:bg-${color}-500 hover:text-white`}
+                className={`border-2 p-2 rounded-lg ${color === 'red' ? 'bg-red-400 hover:bg-red-500' : color === 'blue' ? 'bg-blue-400 hover:bg-blue-500' : 'bg-green-400 hover:bg-green-500'} hover:text-white`}
                 onClick={onClick}
             >
                 {title}
--- client/src/components/text-translator.component.tsx
+++ client/src/components/text-translator.component.tsx
@@ -12,34 +12,35 @@ const TextTranslator : React.FC<TextTranslatorProps> = ({ inputLanguage, inputTe
   const uniquePhrase = getTop3UniquePhrases(outputText);
   const frequencyWord = getFrequencyWord(outputText);
 
-  useEffect(() => {
-    function TranslatedText() {
-      const maxChunkLength = 1000; // Define the maximum length of each chunk
+  function TranslatedText() {
+    const maxChunkLength = 1000; // Define the maximum length of each chunk
 
-      // Split the input text into chunks
-      const textChunks = [];
-      for (let i = 0; i < inputText.length; i += maxChunkLength) {
-        textChunks.push(inputText.substr(i, maxChunkLength));
-      }
-
-      // Translate each chunk and store promises in an array
-      const translationPromises = textChunks.map(chunk => {
-        const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${inputLanguage}&tl=en-US&dt=t&q=${encodeURI(chunk)}`;
-
-        return fetch(url)
-          .then(response => response.json())
-          .then(json => json[0].map((item:any) => item[0]).join(''));
-      });
-      
-      Promise.all(translationPromises)
-        .then(translations => {
-          const translatedText = translations.join(' ');
-          setoutputText(translatedText);
-        })
-        .catch(error => {
-          console.error("Translation error:", error);
-        });
+    // Split the input text into chunks
+    const textChunks = [];
+    for (let i = 0; i < inputText.length; i += maxChunkLength) {
+      textChunks.push(inputText.substr(i, maxChunkLength));
     }
+
+    // Translate each chunk and store promises in an array
+    const translationPromises = textChunks.map(chunk => {
+      const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${inputLanguage}&tl=en-US&dt=t&q=${encodeURI(chunk)}`;
+
+      return fetch(url)
+        .then(response => response.json())
+        .then(json => json[0].map((item:any) => item[0]).join(''));
+    });
+    
+    Promise.all(translationPromises)
+      .then(translations => {
+        const translatedText = translations.join(' ');
+        setoutputText(translatedText);
+      })
+      .catch(error => {
+        console.error("Translation error:", error);
+      });
+  }
+
+  useEffect(() => {
     TranslatedText();
   }, [inputText]);
 
